a8dbfebeb129a0e88a1e64ab064b6155f6cd4133
frantic-dev <okeyamazing@gmail.com>
2025-02-20 21:17:34 +0100

    add sign in and sign out logic

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,24 +1,28 @@
-import { signInAnonymously, updateProfile } from "firebase/auth"
+import { signInAnonymously, updateProfile, signOut, onAuthStateChanged } from "firebase/auth"
 import { auth } from "../index"
+import { useEffect, useState } from "react"
 
 const SignInForm = () => {
 
+  const [user, setUser] = useState(auth.currentUser)
+
   function signIn(e) {
+    // prevent default page reload 
+    e.preventDefault()
     const usernameInput = document.querySelector('form input')
 
-    e.preventDefault()
-    console.log('its working')
-
+    // sign in if user input is not empty
     if (usernameInput.value !== '') {
       console.log(usernameInput.value)
 
       signInAnonymously(auth).then(async (result) => {
-        console.log('logged in anonymously?')
-        console.log(result)
-        console.log(result.user)
+        console.log('logged in anonymously')
+        // assign display name to user
         await updateProfile(result.user, {
           displayName: usernameInput.value
         })
+        // update user state
+        setUser(result.user)
       }).catch((error) => {
         const errorCode = error.errorCode
         const errorMessage = error.message
@@ -30,13 +34,42 @@ const SignInForm = () => {
 
   }
 
+  function signOutUser() {
+
+    // sign out user
+    signOut(auth).then(() => {
+      console.log('signed out successfully')
+      // update user state to null
+      setUser(null)
+    }).catch((error) => {
+      const errorCode = error.errorCode
+      const errorMessage = error.message
+
+      console.log(errorCode)
+      console.log(errorMessage)
+    })
+  }
+
+  function SignInForm() {
+    return (
+      <form>
+        <label htmlFor="username"> Username
+          <input type="text" id="username" />
+        </label>
+        <button onClick={signIn}>sign in</button>
+      </form>
+
+    )
+
+  }
+
   return (
-    <form>
-      <label htmlFor="username"> Username
-        <input type="text" id="username" />
-      </label>
-      <button onClick={signIn}>Submit</button>
-    </form>
+    <>
+      {/* show sign in form if there is no current user and show sign out button if user is logged in */}
+      {user === null ? <SignInForm /> : <button onClick={signOutUser}>sign out</button>}
+
+    </>
+
   )
 }
 
